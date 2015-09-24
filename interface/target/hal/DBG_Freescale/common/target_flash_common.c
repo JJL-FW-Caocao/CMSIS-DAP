f2a553a7f7bc1702d61027672f8c3b127174147d
Michael Hennerich <michael.hennerich@analog.com>
2015-09-24 16:59:21 +0200

    interface/target/hal/DBG_Freescale/common/target_flash_common: Exclude FLASH SEC check
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

--- interface/target/hal/DBG_Freescale/common/target_flash_common.c
+++ interface/target/hal/DBG_Freescale/common/target_flash_common.c
@@ -89,7 +89,8 @@ uint8_t target_flash_program_page(uint32_t addr, uint8_t * buf, uint32_t size)
 {
     uint32_t bytes_written = 0;
 
-#ifndef DBG_ADUCM360
+
+#if !defined(DBG_ADUCM360)
     // call a target dependent function to check if
     // we don't want to write specific bits (flash security bits, ...)
     if (!check_security_bits(addr, buf)) {
